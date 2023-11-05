c61f72e2c612a1a7ebd6e4e0a70c2561aa0feca1
zeynab badji <s.b42@zig.univ.sn>
2023-11-05 18:37:58 +0000

    Ajout de event-front dans mon fork

---
+++ event-front/src/js/App.js
@@ -0,0 +1,18 @@
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div id='App'>
+      <Navbar />
+      <div>
+        Main
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
